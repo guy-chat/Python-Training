b70571af035747c0cd26fc2a63595ea117f3649b
guychat97 <bhumipat.c@gmail.com>
2021-03-12 18:51:46 +0000

    Remove

--- list.py
+++ list.py
@@ -5,6 +5,12 @@ print(bic[1])
 print(bic[-1])
 
 car = ['toyota','honda','ferrari']
-print(car)
+car.append('mistu')
 car[1] = 'subaru' #this replaces 'honda' with 'subaru'
+car.insert(1,'ducatti') #insert 'ducatti' as 2nd elem
+del car[1] #delete second elem in car list
 print(car)
+
+car1 = ['toyota','honda','ferrari']
+second_owned = car1.pop(1)
+print('The second car I owned is a ' + second_owned.title() +'.')
