c97e05d467df80f26d76f831c91794ce68ffbf21
guy-chat <62842728+guy-chat@users.noreply.github.com>
2021-03-14 14:05:05 +1100

    Update list.py

--- list.py
+++ list.py
@@ -14,3 +14,27 @@ print(car)
 car1 = ['toyota','honda','ferrari']
 second_owned = car1.pop(1)
 print('The second car I owned is a ' + second_owned.title() +'.')
+
+dinner = ['kate','gunn','kaye']
+print('Come to dinner ' + dinner[0].title() + '!')
+print('Come to dinner ' + dinner[1].title() + '!')
+print('Come to dinner ' + dinner[2].title() + '!')
+
+bailer = dinner.pop(1).title()
+print('come to dinner ' + dinner[1].title() + '!')
+print(bailer + ' is no longer coming to dinner!')
+dinner.insert(1,'mum')
+print('but ' +dinner[1].title() + ' is coming instead!')
+dinner[0] = 'pun'
+dinner[1] = 'kim'
+dinner[2] = 'net'
+print('come to dinner ' + dinner[0].title() + '!')
+
+dinner.insert(0,'syrup')
+dinner.insert(len(dinner)//2,'france')
+dinner.append('phoon')
+print(dinner)
+del dinner[-1]
+del dinner[-1]
+print(dinner)
+
